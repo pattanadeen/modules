8389c9e05de64f8f93d8105d134f8e7dc0bf7d45
minchayisara <chayisara.sakunkoo.ug@dartmouth.edu>
2021-10-10 22:49:57 -0400

    null

--- queue.c
+++ queue.c
@@ -40,27 +40,27 @@ queue_t* qopen(void){
     return ((queue_t*)qs);
 }
 
-void fn(void *elementp) {
-    car_t *carp = (car_t*)elementp;
-    carp->price = 420.01;
-}
-
 int32_t qput(queue_t *qp, void *elementp){
-    if(((queue_s *)qp)->back == NULL){
-        struct node *newNode = malloc(sizeof(node_t));
-        newNode->next = NULL;
-        newNode->element = elementp;
-        ((queue_s *)qp)->back = newNode;
-        ((queue_s *)qp)->front = newNode;
-        return 0;
+    if(qp == NULL || elementp == NULL){
+        return 1;
     }
     else{
-        struct node *newNode = malloc(sizeof(node_t));
-        newNode->next = NULL;
-        newNode->element = elementp;
-        (((queue_s *)qp)->back)->next = newNode;
-        ((queue_s *)qp)->back = newNode;
-        return 0;
+        if(((queue_s *)qp)->back == NULL){
+            struct node *newNode = malloc(sizeof(node_t));
+            newNode->next = NULL;
+            newNode->element = elementp;
+            ((queue_s *)qp)->back = newNode;
+            ((queue_s *)qp)->front = newNode;
+            return 0;
+        }
+        else{
+            struct node *newNode = malloc(sizeof(node_t));
+            newNode->next = NULL;
+            newNode->element = elementp;
+            (((queue_s *)qp)->back)->next = newNode;
+            ((queue_s *)qp)->back = newNode;
+            return 0;
+        }
     }
 }
 
@@ -72,9 +72,9 @@ void* qget(queue_t *qp){
     return newNode->element;
 }
 
-void qapply(queue_t *qp, void (*fn)(void* elementp)){
-    struct node *newNode = malloc(sizeof(node_t));
-    for (newNode = (node_t*)(((queue_s *)qp)->front); newNode != NULL; newNode = newNode->next) {
-        fn(newNode->element);
-    }
-}
+// void qapply(queue_t *qp, void (*fn)(void* elementp)){
+//     struct node *newNode = malloc(sizeof(node_t));
+//     for (newNode = (node_t*)(((queue_s *)qp)->front); newNode != NULL; newNode = newNode->next) {
+//         fn(newNode->element);
+//     }
+// }
