5d1ec3719b32f5139e22f9ce8c7e3b6d7b76eb0c
minchayisara <chayisara.sakunkoo.ug@dartmouth.edu>
2021-10-10 20:31:50 -0400

    yo

--- queue.c
+++ queue.c
@@ -16,12 +16,12 @@
 
 #define MAXREG 10
 
-typedef struct car {
-	struct car *next;
-	char plate[MAXREG];
-	double price;
-	int year;
-}  car_t;
+// typedef struct car {
+// 	struct car *next;
+// 	char plate[MAXREG];
+// 	double price;
+// 	int year;
+// }  car_t;
 
 typedef struct node {
  	struct node *next;
@@ -47,15 +47,25 @@ int32_t qput(queue_t *qp, void *elementp){
     // printf("%p\n",&qp);
     // printf("%p\n",back);
     // printf("success %p &back\n",&back);
-    printf("success for real\n");
-    struct node *newNode = malloc(sizeof(*newNode));
-    newNode->next = NULL;
-    newNode->element = elementp;
-    (((queue_s *)qp)->back)->next = newNode;
-    ((queue_s *)qp)->back = newNode;
-        // printf("%p\n",&newNode);
-    free(newNode);
-    return 0;
+    if(((queue_s *)qp)->back == NULL){
+        struct node *newNode = malloc(sizeof(*newNode));
+        newNode->next = NULL;
+        newNode->element = elementp;
+        ((queue_s *)qp)->back = newNode;
+        ((queue_s *)qp)->front = newNode;
+        return 0;
+    }
+    else{
+        printf("success for real\n");
+        struct node *newNode = malloc(sizeof(*newNode));
+        newNode->next = NULL;
+        newNode->element = elementp;
+        (((queue_s *)qp)->back)->next = newNode;
+        ((queue_s *)qp)->back = newNode;
+            // printf("%p\n",&newNode);
+        free(newNode);
+        return 0;
+    }
 }
 
 void* qget(queue_t *qp){
@@ -64,51 +74,51 @@ void* qget(queue_t *qp){
     return NULL;
 }
 
-static car_t *make_car(char *platep,double price,int year) { 
-    car_t *cc;
-	if(!(cc = (car_t*)malloc(sizeof(car_t)))) {
-	    printf("[Error: malloc failed allocating car]\n");
-        return NULL;
-    }
-	cc->next = NULL;
-    strcpy(cc->plate,platep);
-    cc->price=price;
-    cc->year=year;
-    return cc;
-}
+// static car_t *make_car(char *platep,double price,int year) { 
+//     car_t *cc;
+// 	if(!(cc = (car_t*)malloc(sizeof(car_t)))) {
+// 	    printf("[Error: malloc failed allocating car]\n");
+//         return NULL;
+//     }
+// 	cc->next = NULL;
+//     strcpy(cc->plate,platep);
+//     cc->price=price;
+//     cc->year=year;
+//     return cc;
+// }
 
- int main(int argc, char *argv[]) {
-    // printf("%p qp\n",qp);
-    // printf("%p &qp\n",&qp);
-    // printf("%p back null\n",back);
-    // printf("%p &back\n",&back);
-    queue_t *qp = qopen();
-    // printf("%p qp\n",qp);
-    // printf("%p &qp\n",&qp);
-    car_t *test1 = make_car("Bill",19000,2020);
-    car_t *test2 = make_car("Sam",21000.75,2019);
-    car_t *test3 = make_car("John",20000.25,2018);
-    car_t *test4 = make_car("Fred",20000,2021);
+//  int main(int argc, char *argv[]) {
+//     // printf("%p qp\n",qp);
+//     // printf("%p &qp\n",&qp);
+//     // printf("%p back null\n",back);
+//     // printf("%p &back\n",&back);
+//     queue_t *qp = qopen();
+//     // printf("%p qp\n",qp);
+//     // printf("%p &qp\n",&qp);
+//     car_t *test1 = make_car("Bill",19000,2020);
+//     car_t *test2 = make_car("Sam",21000.75,2019);
+//     car_t *test3 = make_car("John",20000.25,2018);
+//     car_t *test4 = make_car("Fred",20000,2021);
     
-    // struct node *node1;
-    // node1->next = NULL;
-    // node1->element = test1;
+//     // struct node *node1;
+//     // node1->next = NULL;
+//     // node1->element = test1;
 
-    // ((queue_s *)qp)->front = node1;
-    // ((queue_s *)qp)->back = node1;
+//     // ((queue_s *)qp)->front = node1;
+//     // ((queue_s *)qp)->back = node1;
 
-    // printf("%p back node1\n", back);
-    printf("%p qp1\n",((queue_s *)qp)->back);
-    int ans = qput(qp, &test2);
-    printf("%d\n", ans);
-    printf("%p qp2\n",((queue_s *)qp)->back);
-    // void *elementpp = qget(qp);
-    // struct node *testnode = back;
-    // car_t *testcar = (car_t*)testnode->element;
-    // printf("%d",testcar->year);
-    free(test1);
-    free(test2);
-    free(test3);
-    free(test4);
-    exit(EXIT_SUCCESS);
-}
+//     // printf("%p back node1\n", back);
+//     printf("%p qp1\n",((queue_s *)qp)->back);
+//     int ans = qput(qp, &test2);
+//     printf("%d\n", ans);
+//     printf("%p qp2\n",((queue_s *)qp)->back);
+//     // void *elementpp = qget(qp);
+//     // struct node *testnode = back;
+//     // car_t *testcar = (car_t*)testnode->element;
+//     // printf("%d",testcar->year);
+//     free(test1);
+//     free(test2);
+//     free(test3);
+//     free(test4);
+//     exit(EXIT_SUCCESS);
+// }
